34adb5d291d133956460486009337689709f00fe
unknown <sourabhsat89@gmail.com>
2024-04-15 01:33:42 +0530

    form update

--- Google-Sheet.js
+++ Google-Sheet.js
@@ -1,20 +1,22 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbz2lx9oXRrhYe-CedPd4Ev7TBc6BemXu2lzuYDb8eXaTeicbJBHCuGjrkHTSWLY8Suk/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwFGW7tXOHNvymQU3Hp341NoM2Hw5cM3gYunx4GPORjXNBhEMHwcbXN-szuQwqZxVHv/exec'
 
 const form = document.forms['contact-form']
 
 form.addEventListener('submit', e => {
   e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => {
-    if (response.ok) {
-      window.confirm('Thank you! Your form is submitted successfully.', '');
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then(response => {
+      if (response.ok) {
+        
+        window.confirm('Thank you! Your request is submitted successfully.We will reach out to you shortly', '');
 
-      
-      window.location.reload();
-    } else {
-      throw new Error('Network response was not OK');
-    }
-  })
-  .catch(error => console.error('Error!', error.message))
+
+
+        window.location.reload();
+      } else {
+        throw new Error('Network response was not OK');
+      }
+    })
+    .catch(error => console.error('Error!', error.message))
 })
 
